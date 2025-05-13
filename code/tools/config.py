a8045197626cc28a5a95aaf19cd1ba698fc6ab63
Sook13 <2283118171@qq.com>
2025-05-13 14:41:22 +0800

    comit

--- code/tools/config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-model_paths = {
-    'EGV_AttNet':'tools.model',
-    'simplecombine': 'tools.model_simplecombine',
-    'eGe_Net':'tools.model_eGe',
-    'vgg_Net':'tools.model_vgg'
-}
